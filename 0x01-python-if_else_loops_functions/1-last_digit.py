4e950e572c289a2c8452cae6d0eb6ad22af22317
abeermosaad <abeermosaad69@gmail.com>
2023-06-06 18:03:11 +0300

    negtive num is done

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-n = number % 10
+if (number < 0):
+    number *= -1
+    n = number % 10
+    n *= -1
+    number *= -1
+else:
+    n = number % 10
+
 if n == 0:
     print(f"Last digit of {number} is 0 and is 0")
 elif n > 5:
