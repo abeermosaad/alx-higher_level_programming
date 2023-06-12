0eabcd644a9eca5168b0874c41e25b25d6d957ae
abeermosaad <abeermosaad69@gmail.com>
2023-06-12 18:28:20 +0300

    update adv

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, div, mul, sub
     len = len(argv) - 1
     op = ['+', '-', '/', '*']
     if len != 3:
@@ -11,14 +12,18 @@ if __name__ == "__main__":
         exit(1)
     else:
         if argv[2] == '+':
-            print('{} + {} = {}'
-                  .format(int(argv[1]), argv[3], int(argv[1]) + int(argv[3])))
+            print('{} + {} = {}'.format
+                  (int(argv[1]), argv[3], add(int(argv[1]), int(argv[3]))))
+            exit(0)
         elif argv[2] == '-':
-            print("{} + {} = {}"
-                  .format(int(argv[1]), argv[3], int(argv[1]) - int(argv[3])))
+            print("{} + {} = {}".format
+                  (int(argv[1]), argv[3], sub(int(argv[1]), int(argv[3]))))
+            exit(0)
         elif argv[2] == '*':
-            print("{} + {} = {}"
-                  .format(int(argv[1]), argv[3], int(argv[1]) * int(argv[3])))
+            print("{} + {} = {}".format
+                  (int(argv[1]), argv[3], mul(int(argv[1]), int(argv[3]))))
+            exit(0)
         else:
-            print("{} + {} = {}"
-                  .format(int(argv[1]), argv[3], int(argv[1]) / int(argv[3])))
+            print("{} + {} = {}".format
+                  (int(argv[1]), argv[3], div((argv[1]), int(argv[3]))))
+            exit(0)
