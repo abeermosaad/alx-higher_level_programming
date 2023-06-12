78b87637bccde472991ebd8b51541db65e888a86
abeermosaad <abeermosaad69@gmail.com>
2023-06-12 19:28:01 +0300

    updatttttttttte adv 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     from calculator_1 import add, div, mul, sub
     len = len(argv) - 1
     op = ['+', '-', '/', '*']
+    fun = [add, sub, div, mul]
     if len != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -11,19 +12,9 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        if argv[2] == '+':
-            print('{} + {} = {}'.format
-                  (int(argv[1]), argv[3], add(int(argv[1]), int(argv[3]))))
-            exit(0)
-        elif argv[2] == '-':
-            print("{} + {} = {}".format
-                  (int(argv[1]), argv[3], sub(int(argv[1]), int(argv[3]))))
-            exit(0)
-        elif argv[2] == '*':
-            print("{} + {} = {}".format
-                  (int(argv[1]), argv[3], mul(int(argv[1]), int(argv[3]))))
-            exit(0)
-        else:
-            print("{} + {} = {}".format
-                  (int(argv[1]), argv[3], div((argv[1]), int(argv[3]))))
-            exit(0)
+        for i in range(4):
+            if argv[2] == op[i]:
+                print('{} + {} = {}'.format
+                      (int(argv[1]), argv[3],
+                       fun[i](int(argv[1]), int(argv[3]))))
+                exit(0)
