02636302109bcc31f839fb5c7e4bda18faaa3e21
abeermosaad <abeermosaad69@gmail.com>
2023-07-07 02:59:53 +0300

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+
+def text_indentation(text):
+    
+	if not isinstance(text, str):
+		raise TypeError("text must be a string")
+	text1 = ""
+	char_list = ['.', '?', ':']
+	text1 = text.strip(" ")
+
+	for i in range(len(text1)):
+		if (text1[i] in char_list):
+			print(text1[i], end="\n\n")
+		elif (text1[i] != " " and (text1[i - 1] != " ") or text1[i - 1] not in char_list):
+			print(text1[i], end="")
+
+    
