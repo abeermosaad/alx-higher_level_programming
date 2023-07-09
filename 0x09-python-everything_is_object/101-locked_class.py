9a85f0d33b553fc73d09367cfd9a347cacd13dc0
abeermosaad <abeermosaad69@gmail.com>
2023-07-09 04:33:33 +0300

    adv 2

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""LockedClass"""
+
+
+class LockedClass:
+
+    __slots__ = ["first_name", "last_name"]
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
