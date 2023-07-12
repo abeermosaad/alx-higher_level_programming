39463b81fe2de0cc2b5501f7218a9513866d7dc3
abeermosaad <abeermosaad69@gmail.com>
2023-07-12 21:19:49 +0300

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""load_from_json_file function"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file””"""
+    return (json.load(filename))
