73813bc1ef9205e30f51b4cc68ee15e97ada5236
abeermosaad <abeermosaad69@gmail.com>
2023-09-12 01:22:59 +0300

    Task 11 again

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,10 @@ let len = process.argv.length; const arr = [];
 if (len === 2 || len === 3) {
   console.log(0);
 } else {
-	for (let i = 2; i < len; i++) {
-		arr.push(parseInt(process.argv[i]));
-	  }
-	  len = arr.length;
-	  arr.sort((a, b) => a - b);
-	  console.log(arr[len - 2]);
+  for (let i = 2; i < len; i++) {
+    arr.push(parseInt(process.argv[i]));
+  }
+  len = arr.length;
+  arr.sort((a, b) => a - b);
+  console.log(arr[len - 2]);
 }
