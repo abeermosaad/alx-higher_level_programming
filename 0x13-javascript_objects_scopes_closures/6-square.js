2c7eeb930547ca17999a08bc314e3099926791e0
abeermosaad <abeermosaad69@gmail.com>
2023-09-13 02:32:26 +0300

    Tasks 5 & 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c === undefined) { console.log('X'.repeat(this.width)); } else { console.log(c.repeat(this.height)); }
+    }
+  }
+}
+module.exports = Square;
