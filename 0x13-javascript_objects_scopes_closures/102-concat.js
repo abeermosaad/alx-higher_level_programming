a4fa14120fbe2383246bf6228b5991be02dad995
abeermosaad <abeermosaad69@gmail.com>
2023-09-13 05:36:10 +0300

    adv 3 again

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
-let fs = require('fs');
-let data;
-fs.readFile(process.argv[2], 'utf8', function(error, data1){
-	fs.writeFile(process.argv[4], data1, 'utf8', (err) => {
-		if (err) {
-		  console.error('Error writing to fileC:', err);
-		  return;
-		}})
-})
-fs.readFile(process.argv[3], 'utf8', function(error, data1){
-	fs.appendFile(process.argv[4], data1, 'utf8', (err) => {
-		if (err) {
-		  console.error('Error writing to fileC:', err);
-		  return;
-		}})
-})
+const fs = require('fs');
 
+fs.readFile(process.argv[2], 'utf8', function (error, data1) {
+  fs.writeFile(process.argv[4], data1, 'utf8', (err) => {
+    if (err) {
+      console.error(error);
+    }
+  });
+});
 
+fs.readFile(process.argv[3], 'utf8', function (error, data1) {
+  fs.appendFile(process.argv[4], data1, 'utf8', (err) => {
+    if (err) {
+      console.error(error);
+    }
+  });
+});
