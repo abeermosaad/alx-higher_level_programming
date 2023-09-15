c44a54547c91450f67e5a5ba3f34d2d3a6e3ed7e
abeermosaad <abeermosaad69@gmail.com>
2023-09-15 21:46:25 +0300

    pycodestyle has been applied

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,11 @@ mydb = MySQLdb.connect(
     host='localhost',
     user='root',
     passwd='root',
-	db = 'hbtn_0e_0_usa'
+    db='hbtn_0e_0_usa'
 )
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM states ORDER BY ID ASC")
 
 result = mycursor.fetchall()
 for row in result:
-	print (row)
+    print(row)
