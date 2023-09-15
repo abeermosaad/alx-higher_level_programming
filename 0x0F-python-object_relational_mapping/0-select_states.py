3a2e352404ea33719e003f5f48858205ce6451e0
abeermosaad <abeermosaad69@gmail.com>
2023-09-15 21:40:34 +0300

    Task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+
+mydb = MySQLdb.connect(
+    host='localhost',
+    user='root',
+    passwd='root',
+	db = 'hbtn_0e_0_usa'
+)
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM states ORDER BY ID ASC")
+
+result = mycursor.fetchall()
+for row in result:
+	print (row)
