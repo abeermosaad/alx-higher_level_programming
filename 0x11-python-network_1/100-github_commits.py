68054b7d36f49e09911dafe7f68bbe2005976c95
abeermosaad <abeermosaad69@gmail.com>
2023-10-01 01:39:13 +0300

    Adv 1

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""takes your GitHub credentials uses the GitHub API to display your id"""
+
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    import json
+
+    response = requests.get(
+        f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits')
+
+    resp = response.json()
+    for i in range(11):
+        n = resp[i]["commit"]["author"]["name"]
+        # response2 = requests.get(f'https://api.github.com/users/{n}')
+        # resp2 = response2.json()
+        print(f'{resp[i].get("sha")}: {n}')
