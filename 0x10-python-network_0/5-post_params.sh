e9fd5d5ec17c8bb43842003f4cad12ad05f1e172
abeermosaad <abeermosaad69@gmail.com>
2023-09-30 14:36:28 +0300

    Task 5 again

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A header variable X-School-User-Id must be sent with the value 98
-curl -sL -X POST -d "email: test@gmail.com&subject: I will always be here for PLD" "$1"
+curl -sL -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
