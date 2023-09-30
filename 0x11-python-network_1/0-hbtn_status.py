5346caec7f67b3eb0fa740b2fac26ba17100108d
abeermosaad <abeermosaad69@gmail.com>
2023-09-30 15:15:25 +0300

    Task 0 again

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,12 @@
 from urllib import request
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        resp = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(resp)))
-        print("\t- content: {}".format(resp))
-        print("\t- utf8 content: {}".format(resp.decode('utf-8')))
+    try:
+        with request.urlopen('https://intranet.hbtn.io/status') as response:
+            resp = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(resp)))
+            print("\t- content: {}".format(resp))
+            print("\t- utf8 content: {}".format(resp.decode('utf-8')))
+    except:
+        pass
