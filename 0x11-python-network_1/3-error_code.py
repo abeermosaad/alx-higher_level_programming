8c869909a23f2dfa9a52c187c6568120b6290f6a
abeermosaad <abeermosaad69@gmail.com>
2023-09-30 17:36:44 +0300

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" manage urllib.error.HTTPError"""
+
+
+if __name__ == '__main__':
+    from urllib import request, error
+    import sys
+
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            resp = response.read()
+            print(resp.decode('utf-8'))
+    except error.HTTPError as e:
+        print(e.code)
